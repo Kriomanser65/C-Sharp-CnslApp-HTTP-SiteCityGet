ce7235fda99b78b0f0b96ed181baed923cc6e8e8
Stekoks <Stekoks@DESKTOP-HSE7N52>
2023-12-27 01:29:17 +0200

    Add project files.

---
+++ bank/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string city = "Дніпропетровськ";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string apiUrl = $"https://api.privatbank.ua/p24api/pboffice?city={city}"; //{"response_code":200,"response_description":"The service is closed since August 31, 2022"}
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(responseBody);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"HTTP Error: {response.StatusCode}");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request exception: {e.Message}");
+                }
+            }
+        }
+    }
+}
